1250e9c3139a65d86a52ce667fc8df45499b3a89
neillturner <neillwturner@gmail.com>
2018-05-28 12:41:41 +0100

    autospotting

--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
-output "function_name" {
-  value = "${element(concat(aws_lambda_function.autospotting.*.function_name, aws_lambda_function.autospotting_from_s3.*.function_name), 0)}"
-}
-
-output "arn" {
-  value = "${element(concat(aws_lambda_function.autospotting.*.arn, aws_lambda_function.autospotting_from_s3.*.arn), 0)}"
-}
-
 output "autospotting_role_name" {
   value = "${aws_iam_role.autospotting_role.name}"
 }
